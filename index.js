1613b84a2415bddc2da54cbe728d96a4d1d428c2
Guilherme Lopes da Silva ( Lobus ) <107316736+guisilva10@users.noreply.github.com>
2023-10-16 23:24:24 -0300

    Update index.js
    add api twilio and send message

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_API_KEY);
 const express = require("express");
 const cors = require("cors");
+const accountSid = 'AC9c03229435bcdf369efb66ee8e982617';
+const authToken = 'e1ed3ff45e1ecd45453ec6b1944ce136';
+const client = require('twilio')(accountSid, authToken);
 
 const app = express();
+
+
+
 
 app.use(cors());
 app.use(express.json());
 
+ 
+    
 const PAYMENT_CONFIRMATION_URL = `${process.env.FRONT_END_URL}/payment-confirmation`;
 
 app.post("/create-checkout-session", async (req, res) => {
@@ -30,13 +38,37 @@ app.post("/create-checkout-session", async (req, res) => {
     mode: "payment",
     success_url: `${PAYMENT_CONFIRMATION_URL}?success=true`,
     cancel_url: `${PAYMENT_CONFIRMATION_URL}?canceled=true`,
-    
+   
+
   });
 
- 
+  // Enviar informações do pedido para o WhatsApp do administrador
+  const pedidoInfo = items.map((item) => `${item.price_data.product_data.name} - Quantidade: ${item.quantity} - Preço Total: ${item.price_data.unit_amount}`);
+  const pedidoMensagem = `Você recebeu um novo pedido:\n${pedidoInfo.join('\n')}`;
+
+
+function enviarPedido(){
+  return (
+    client.messages
+  .create({
+    body: pedidoMensagem,
+    from: '+14344855434',
+    to: '+5511948798912', // Seu número de WhatsApp
+  })
+  .then((message) => console.log(`Mensagem enviada com SID: ${message.sid}`))
+  )
+}
+enviarPedido(session.success_url)
+
+
+
+    
+
 
   res.send({ url: session.url });
 });
 
 app.listen(process.env.PORT || 5000);
+
+
 
